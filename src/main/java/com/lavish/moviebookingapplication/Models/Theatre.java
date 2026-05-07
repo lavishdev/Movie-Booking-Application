ffb9a6ef667234934d9da80378679d8f0d2cc01c
lavishdev <saini.lavish2007@gmail.com>
2026-05-07 12:44:59 +0530

    feat: implemented theatre service, model, repo, controller

--- src/main/java/com/lavish/moviebookingapplication/Models/Theatre.java
+++ src/main/java/com/lavish/moviebookingapplication/Models/Theatre.java
@@ -1,4 +1,26 @@
 package com.lavish.moviebookingapplication.Models;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Data
 public class Theatre {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String theatreName;
+    private String theatreLocation;
+    private Integer theatreCapacity;
+    private String theatreScreenType;
+
+    @OneToMany(mappedBy = "theatre", fetch = FetchType.LAZY)
+    private List<Show> shows;
+
+
+
 }
